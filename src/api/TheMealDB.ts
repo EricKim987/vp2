25059814fe0d09a6904fb6eb34563725db8f4565
EricKim987 <ericjin987@gmail.com>
2025-07-03 13:19:05 +0800

    Add Mock Service Worker

--- src/api/TheMealDB.ts
+++ src/api/TheMealDB.ts
@@ -1,40 +1,43 @@
-const base_url = "https://www.themealdb.com/api/json/v1/1"
+const base_url = "https://www.themealdb.com/api/json/v1/1";
+
+export const getCategoriesUrl = `${base_url}/categories.php`;
+export const getMealsByCategoryUrl = `${base_url}/filter.php`;
 
 export type CategoryType = {
-  idCategory:string,
-  strCategory	:string,
-  strCategoryThumb	: string,
-  strCategoryDescription	:string,
-}
+	idCategory: string;
+	strCategory: string;
+	strCategoryThumb: string;
+	strCategoryDescription: string;
+};
 
 export type CategoriesType = {
-  categories	: CategoryType[]
-}
+	categories: CategoryType[];
+};
 
-export async function getCategories():Promise<CategoriesType> {
-  const response = await fetch(`${base_url}/categories.php`)
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`)
-  }
-  const data = await response.json()
-  return data
+export async function getCategories(): Promise<CategoriesType> {
+	const response = await fetch(getCategoriesUrl);
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+	const data = await response.json();
+	return data;
 }
 
 export type MealType = {
-  strMeal: string,
-  strMealThumb: string,
-  idMeal: string,
-}
+	strMeal: string;
+	strMealThumb: string;
+	idMeal: string;
+};
 
 export type MealsType = {
-  meals: MealType[]
-}
+	meals: MealType[];
+};
 
-export async function getMealsByCategory(category: string):Promise<MealsType> {
-  const response = await fetch(`${base_url}/filter.php?c=${category}`)
-  if (!response.ok) {
-    throw new Error(`Response status: ${response.status}`)
-  }
-  const data = await response.json()
-  return data
+export async function getMealsByCategory(category: string): Promise<MealsType> {
+	const response = await fetch(`${getMealsByCategoryUrl}?c=${category}`);
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+	const data = await response.json();
+	return data;
 }
--- src/views/Home/HomeView.test.ts
+++ src/views/Home/HomeView.test.ts
@@ -1,10 +1,34 @@
 import HomeView from "./HomeView.vue";
-import {render, screen} from '@testing-library/vue'
+import { render, screen } from "@testing-library/vue";
+import { VueQueryPlugin } from "@tanstack/vue-query";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
+import { getCategoriesUrl, type CategoriesType } from "@/api/TheMealDB";
+
+const categories: CategoriesType = {
+	categories: [
+		{
+			idCategory: "1",
+			strCategory: "test",
+			strCategoryThumb: "test-img",
+			strCategoryDescription: "test-description",
+		},
+	],
+};
+
+const categoriesHandler = http.get(getCategoriesUrl, () => {
+	return HttpResponse.json(categories);
+});
+
+const server = setupServer(categoriesHandler);
 
 describe("HomeView", () => {
-  it('renders title', () => {
-    render(HomeView)
-    const title = screen.getByText("Recipes")
-    expect(title).toBeInTheDocument()
-  })
-})
+	beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+	afterAll(() => server.close());
+	afterEach(() => server.resetHandlers());
+	it("renders title", () => {
+		render(HomeView, { global: { plugins: [VueQueryPlugin] } });
+		const title = screen.getByText("Recipes");
+		expect(title).toBeInTheDocument();
+	});
+});
