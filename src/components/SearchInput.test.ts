d9cd675718aa669f6b7e68769b9e97218119b767
EricKim987 <ericjin987@gmail.com>
2025-07-05 15:23:43 +0800

    refactor: add SearchInput component

---
+++ src/components/SearchInput.test.ts
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/vue";
+import SearchInput from "./SearchInput.vue";
+import userEvent from "@testing-library/user-event";
+import { useRouter } from "vue-router";
+
+const push = vi.fn();
+vi.mock("vue-router", () => ({
+	useRouter: vi.fn(() => ({
+		push,
+	})),
+}));
+
+describe("SearchInput", () => {
+	beforeEach(() => {
+		push.mockClear();
+		render(SearchInput);
+	});
+
+	it("searches recipes", async () => {
+		const user = userEvent.setup();
+		const input = screen.getByLabelText("Search recipes");
+
+		await user.click(input);
+		await user.keyboard("{Enter}");
+
+		expect(push).not.toHaveBeenCalled();
+
+		await user.type(input, "something");
+		await user.keyboard("{Enter}");
+
+		expect(push).toHaveBeenCalledOnce();
+
+		const searchButton = screen.getByLabelText("search");
+		await user.click(searchButton);
+
+		expect(push).toHaveBeenCalledTimes(2);
+	});
+});
--- src/views/Search/SearchView.vue
+++ src/views/Search/SearchView.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import SearchInput from '@/components/SearchInput.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+</script>
+
 <template>
-  <h1>Search Results</h1>
+  <h1>Search Results {{ $route.query }}</h1>
+  <SearchInput />
 </template>
