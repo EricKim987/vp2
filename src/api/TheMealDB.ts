1198be2d65dcf74eaeb4043ab2b23c07114751f1
EricKim987 <ericjin987@gmail.com>
2025-07-06 20:00:24 +0800

    Add get meal details function

--- src/api/TheMealDB.ts
+++ src/api/TheMealDB.ts
@@ -2,6 +2,7 @@ const base_url = "https://www.themealdb.com/api/json/v1/1";
 
 export const getCategoriesUrl = `${base_url}/categories.php`;
 export const getMealsByCategoryUrl = `${base_url}/filter.php`;
+export const getMealDetailsUrl = `${base_url}/lookup.php`;
 
 export type CategoryType = {
 	idCategory: string;
@@ -46,6 +47,15 @@ export async function getMealsByIngredient(
 	ingredient: string,
 ): Promise<MealsType> {
 	const response = await fetch(`${getMealsByCategoryUrl}?i=${ingredient}`);
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+	const data = await response.json();
+	return data;
+}
+
+export async function getMealDetails(mealId: string): Promise<MealsType> {
+	const response = await fetch(`${getMealDetailsUrl}?i=${mealId}`);
 	if (!response.ok) {
 		throw new Error(`Response status: ${response.status}`);
 	}
