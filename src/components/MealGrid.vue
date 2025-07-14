45eac3c84a7e49cf56957b92fcc59f055b78e4f3
EricKim987 <ericjin987@gmail.com>
2025-07-14 19:57:25 +0800

    Add save button to MealGrid

--- src/components/MealGrid.vue
+++ src/components/MealGrid.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
 
 const { meals } = defineProps<{
   meals: Array<{
@@ -13,15 +12,30 @@ const { meals } = defineProps<{
 
 <template>
   <div class="meals-container">
-    <router-link v-for="meal in meals"
-                 :key="meal.idMeal"
-                 :to="`/recipe/${meal.idMeal}`"
-                 class="meal">
+    <div v-for="meal in meals"
+         :key="meal.idMeal"
+         :to="`/recipe/${meal.idMeal}`"
+         class="meal">
       <img :src="imgSuffix ? `${meal.strMealThumb}/${imgSuffix}` : meal.strMealThumb"
            :alt="meal.strMeal"
            class="meal-image">
-      <div class="meal-text">{{ meal.strMeal }}</div>
-    </router-link>
+      <div class="meal-info">
+        <router-link class="meal-text"
+                     :to="`/recipe/${meal.idMeal}`">{{ meal.strMeal }}</router-link>
+        <button class="meal-save-btn">
+          <svg aria-hidden="true"
+               class="save-btn-icon"
+               xmlns="http://www.w3.org/2000/svg"
+               width="13"
+               height="17.3"
+               viewBox="0 0 13 17.3">
+            <path fill="currentColor"
+                  stroke="var(--color-text)"
+                  d="M12.5,16.5l-6-2.8l-6,2.8v-16h12V16.5z"></path>
+          </svg>
+          <span>Save</span></button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +52,9 @@ const { meals } = defineProps<{
   box-shadow: 0 0 20px var(--color-box-shadow);
   text-decoration: none;
   padding-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+
 }
 
 .meal-image {
@@ -49,10 +66,43 @@ const { meals } = defineProps<{
   opacity: 0.9;
 }
 
+.meal-info {
+  flex: 1;
+  display: flex;
+  padding: 10px 10px 12px 10px;
+  gap: 10px;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
 .meal-text {
-  padding: 5px 10px;
   forced-color-adjust: none;
   color: var(--color-text);
+  text-decoration: none;
+
+  &:hover {
+    color: var(--color-text-hover);
+  }
+}
+
+.meal-save-btn {
+  padding: 5px 12px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    .save-btn-icon {
+      color: var(--color-text);
+      transition: color 0.3s ease;
+    }
+  }
+}
+
+.save-btn-icon {
+  color: transparent;
 }
 </style>
 <!-- Rename this file to MealGrid.vue -->
